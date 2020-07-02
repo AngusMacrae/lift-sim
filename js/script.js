5df3eb4c27fad3201cf51946d30fbdb844d979bc
Angus Macrae <angusdmacrae@gmail.com>
2020-07-02 11:39:46 +0800

    Add drag & drop functionality, and basic styling

--- js/script.js
+++ js/script.js
@@ -9,24 +9,31 @@ class Passenger {
 
 class Building {
   constructor() {
-    this.floors = [new Floor(), new Floor(), new Floor()];
+    this.floors = [new Floor(0), new Floor(1), new Floor(2)];
+    this.element = document.querySelector('.building');
   }
-  addFloor() {
-    this.floors.push(new Floor());
-  }
-  removeFloor() {
-    this.floors.pop();
-  }
-  addLift() {}
-  removeLift() {}
+  // addFloor() {
+  //   let newFloorNumber = this.floors.length;
+  //   this.element.insertAdjacentHTML('afterbegin', '<div class="floor" data-number="' + newFloorNumber + '"><p>New floor</p></div>');
+  //   this.floors.push(new Floor(newFloorNumber));
+  // }
+  // removeFloor() {
+  //   this.floors.pop();
+  // }
+  // addLift() {}
+  // removeLift() {}
 }
 
 class Floor {
-  constructor() {
+  constructor(floorNumber) {
     this.waitingPassengers = [];
+    this.element = document.querySelector('.floor[data-number="' + floorNumber + '"]');
+    this.disembarkArea = this.element.querySelector('.disembark-area');
+    this.waitingArea = this.element.querySelector('.waiting-area');
   }
   addPassenger(destination = 0) {
     this.waitingPassengers.push(new Passenger(destination));
+    this.waitingArea.insertAdjacentHTML('beforeend', '<div class="passenger" data-destination="' + destination + '"><img src="img/passenger.svg" /></div>');
   }
   boardPassenger(id) {
     this.waitingPassengers.filter(waitingPassenger => waitingPassenger.id != id);
@@ -67,6 +74,30 @@ class Lift {
     this.setDestination();
   }
 }
+
+function dragstart_handler(event) {
+  event.dataTransfer.setData('text/plain', event.target.dataset.destination);
+  event.dataTransfer.dropEffect = 'move';
+}
+
+function dragover_handler(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+}
+
+function drop_handler(event) {
+  event.preventDefault();
+  const passengerDestination = event.dataTransfer.getData('text/plain');
+  const floorNumber = event.target.closest('.floor').dataset.number;
+  if (passengerDestination != floorNumber) {
+    const waitingArea = event.target.closest('.waiting-area');
+    const waitingPassengerHTML = '<li class="passenger" data-destination="' + passengerDestination + '"><img src="img/passenger.svg" onload="SVGInject(this)" /></li>';
+    waitingArea.insertAdjacentHTML('beforeend', waitingPassengerHTML);
+  }
+}
+
+let addPassengerDraggables = document.querySelectorAll('.command-palette .passenger');
+addPassengerDraggables.forEach.addEventListener('dragstart', dragstart_handler);
 
 const building = new Building();
 const lift = new Lift();
