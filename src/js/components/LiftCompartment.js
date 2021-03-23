77e01cfd1bf52bb7a5b39344dcae4eff377474ec
Angus Macrae <angusdmacrae@gmail.com>
2021-03-23 00:16:12 +0000

    Change CSS class of LiftCompartment to reflect component name

--- src/js/components/LiftCompartment.js
+++ src/js/components/LiftCompartment.js
@@ -5,7 +5,7 @@ export default class LiftCompartment extends PassengerContainer {
     super();
   }
   render() {
-    return `<ul data-id="${this.id}" class="lift-passengers-container">
+    return `<ul data-id="${this.id}" class="lift-compartment">
               ${this.passengers.map(passenger => passenger.render()).join('')}
             </ul>`;
   }
