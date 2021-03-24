6f152af1722072308b4bae9c81d282b464098605
Angus Macrae <angusdmacrae@gmail.com>
2021-03-24 15:37:53 +0000

    Move lift appropriate distance based on duration of each animation frame (make motion smoother)

--- src/js/components/Lift.js
+++ src/js/components/Lift.js
@@ -8,8 +8,8 @@ export default class Lift extends PassengerContainer {
     this.compartment = new LiftCompartment();
     this.status = 'idle'; // idle, ascending, descending
     this._location = 0;
-    this.maxSpeed = 10; // px/ms
-    this.acceleration = 0.1; // px/ms^2
+    this.maxSpeed = 0.3; // px/ms
+    this.acceleration = 0.0005; // px/ms^2
     this.currentSpeed = 0; // px/ms
     // this.capacity = 9;
   }
@@ -26,35 +26,37 @@ export default class Lift extends PassengerContainer {
     return locationToFloorNumber(this.location, this.ascending);
   }
   get stoppingDistance() {
-    return this.currentSpeed ** 2 / (2 * this.acceleration) + 1;
+    return this.currentSpeed ** 2 / (2 * this.acceleration);
   }
   get location() {
     return this._location;
   }
   set location(newLocation) {
     this._location = newLocation;
-    this.element.style.transform = `translateY(-${this.location}px)`;
+    this.element.style.transform = `translateY(-${newLocation}px)`;
   }
   async goToFloor(floorNum) {
     const targetLocation = floorNumberToLocation(floorNum);
-    let start;
+    let lastTimestamp;
     return new Promise(resolve => {
-      const animateMovement = timestamp => {
+      const animateMovement = currentTimestamp => {
         const arrivedAtTarget = this.ascending ? this.location > targetLocation : this.location < targetLocation;
         if (!arrivedAtTarget) {
+          if (lastTimestamp === undefined) lastTimestamp = currentTimestamp;
+          const frameTime = currentTimestamp - lastTimestamp;
           const distanceToTarget = Math.abs(targetLocation - this.location);
           if (distanceToTarget > this.stoppingDistance) {
-            this.currentSpeed = Math.min(this.maxSpeed, this.currentSpeed + this.acceleration);
+            this.currentSpeed = Math.min(this.maxSpeed, this.currentSpeed + this.acceleration * frameTime);
           } else {
-            this.currentSpeed -= this.acceleration;
+            this.currentSpeed -= this.acceleration * frameTime;
           }
-          if (start === undefined) start = timestamp;
-          const elapsedTime = timestamp - start;
+          // console.log(this.currentSpeed.toFixed(3));
           if (this.ascending) {
-            this.location += this.currentSpeed;
+            this.location += this.currentSpeed * frameTime;
           } else {
-            this.location -= this.currentSpeed;
+            this.location -= this.currentSpeed * frameTime;
           }
+          lastTimestamp = currentTimestamp;
           window.requestAnimationFrame(animateMovement);
         } else {
           this.currentSpeed = 0;
